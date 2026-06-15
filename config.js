251afb1176c4a206f33cc7f968d85357ab398cde
Nathan e Renan <0001151944@senaimgaluno.com.br>
2026-06-15 15:34:00 -0300

    v2

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+const SUPABASE_URL = "https://lnpguegoaxhzkqstpffw.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxucGd1ZWdvYXhoemtxc3RwZmZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1Mzc0ODksImV4cCI6MjA5NzExMzQ4OX0.CtWLTzzBShMmCpqMfAG4j6Ak0KmHHIQN6-_M2DC0-E4";
